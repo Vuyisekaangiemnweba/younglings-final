a7894957cd7600b055c8f3fbe4c0f05bbcc3bd91
Vuyisekaangiemnweba <110176374+Vuyisekaangiemnweba@users.noreply.github.com>
2023-03-07 03:38:07 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+<html>
+<body>
+
+
+<button onclick="myFunction()">Click Me!</button>
+
+<script>
+function myFunction() {
+  let x = document.getElementById("demo");
+  x.style.fontSize = "25px"; 
+  x.style.color = "red"; 
+}
+</script>
+
+
+<script>
+function myFunction() {
+  let x = document.getElementById("demo");
+  x.style.fontSize = "25px"; 
+  x.style.color = "red"; 
+}
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
